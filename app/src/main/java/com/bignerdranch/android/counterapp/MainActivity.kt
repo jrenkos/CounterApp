4b98f55bd4b10269089f9312a7fef1b343f6ea7f
JR <jer993s@missouristate.edu>
2023-02-06 18:18:40 -0600

    FIXED!

--- app/src/main/java/com/bignerdranch/android/counterapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/counterapp/MainActivity.kt
@@ -7,34 +7,29 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var count: Int = 0
-    private var display: TextView? = null
-    private var btn: Button? = null
-
-    class Counter {
-        private var mcount: Int = 0
-
-        fun addCount() {
-            mcount++
-        }
-
-        fun getCount(): Int {
-            return mcount
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        count = Counter().getCount()
-        display = findViewById(R.id.tv1)
-        btn = findViewById(R.id.btn1)
+        val count = Counter()
+        val display = findViewById<TextView>(R.id.tv1)
+        val btn = findViewById<Button>(R.id.btn1)
 
         btn!!.setOnClickListener{
-            Counter().addCount()
+            count.addCount()
 
-            display?.text = count.toString()
+            display?.text = count.getCount().toString()
         }
+    }
+}
+public class Counter {
+    var mcount: Int = 0
+
+    fun addCount() {
+        mcount++
+    }
+
+    fun getCount(): Int {
+        return mcount
     }
 }
